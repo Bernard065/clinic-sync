829fd88394fd2b9666c29cb5f13142d90ebc8f58
+ <+>
2024-07-16 17:05:25 +0300

    change home page image

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
       </section>
 
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/doctor-nurses.jpg"
         height={1000}
         width={1000}
         alt="patient"
